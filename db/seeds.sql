c0a9345f4de149615f571e8a9fae6870008c3fe1
Carolyn Rojsutivat <carolynrojsutivat@carolyns-mbp.attlocal.net>
2018-05-22 10:23:01 -0500

    updated

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name) VALUES ('Big Mac');
+INSERT INTO burgers(burger_name) VALUES ('Quarter Pounder');
+INSERT INTO burgers(burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers(burger_name) VALUES ('Hamburger');
+INSERT INTO burgers(burger_name) VALUES ('Filet O Fish');
